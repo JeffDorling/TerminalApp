e271d813c4e442ba81be3006589eeb3cb15c108b
jeffdorling <jdorling15@gmail.com>
2019-04-23 13:10:58 +1000

    Randomise functions, working

--- game_class.rb
+++ game_class.rb
@@ -20,29 +20,6 @@ class Games
     end
 end
 
-
-#Our List of 20 Games 
-game1 = Games.new("Battlezone 1998", "Real Time Strategy", "A cold war themed battle in space")
-game2 = Games.new("Dead Space", "Horror", "Repair mission in space that goes horribly wrong")
-game3 = Games.new("Mass Effect 2", "RPG", "Recruit a Crew to embark on a suicide mission")
-game4 = Games.new("Total War Shogun 2", "Real Time Strategy", "Rule a Kingdom and fight for the Shogunate")
-game5 = Games.new("Read Dead Redemption", "Action", "It's Grand Theft Auto Cowboys. Yeahaw")
-game6 = Games.new("Gears of War", "Action", "Defend humanity against monsters that live underground")
-game7 = Games.new("Half Life 2", "Action", "Follow Gordon Freeman's fight to save humanity")
-game8 = Games.new("Fallout New Vegas", "RPG", "Find the guy that shot you in the head. Shoot him back")
-game9 = Games.new("Elder Scrolls V Skyrim", "RPG", "As the Dragonborn, kill other dragons")
-game10 = Games.new("The Witcher 3", "RPG", "Stop the Wild Hunt, Play GWENT")
-game11 = Games.new("Call of Duty: Black Ops" , "Action" , "INSERT DESC HERE")
-game12 = Games.new("Call of Duty: Black Ops 2" , "Action" , "INSERT DESC HERE")
-game13 = Games.new("Call of Duty: World at War" , "Action" , "INSERT DESC HERE")
-game14 = Games.new("Outlast" , "Horror" , "INSERT DESC HERE")
-game15 = Games.new("Outlast" , "Horror" , "INSERT DESC HERE")
-game16 = Games.new("The Evil Within 2" , "Horror" , "INSERT DESC HERE")
-game17 = Games.new("Arma 3" , "Action" , "INSERT DESC HERE")
-game18 = Games.new("Squads" , "Action" , "INSERT DESC HERE")
-game19 = Games.new("Rust" , "Survival" , "INSERT DESC HERE")
-game20 = Games.new("Minecraft" , "Survival" , "INSERT DESC HERE")
-
 #Welcome Message Method
 def welcome_message
     outline = "="
@@ -51,4 +28,13 @@ def welcome_message
     puts outline * welcome.length
     puts welcome
     puts outline * welcome.length
+end
+
+#List of Options Method
+def options
+    puts "(a) to add a game"
+    puts "(r) to remove a game"
+    puts "(g) to select list of games by genre"
+    puts "(r) for a random game suggestion"
+    puts "(q) to quit"
 end
--- top_20.rb
+++ top_20.rb
@@ -9,6 +9,32 @@ require 'io/console'
 #Declared Variables 
 outline = "="
 question = "What would you like to do?"
+game_array = []
+#Our List of 20 Games 
+game1 = Games.new("Battlezone 1998", "Real Time Strategy", "A cold war themed battle in space")
+game2 = Games.new("Dead Space", "Horror", "Repair mission in space that goes horribly wrong")
+game3 = Games.new("Mass Effect 2", "RPG", "Recruit a Crew to embark on a suicide mission")
+game4 = Games.new("Total War Shogun 2", "Real Time Strategy", "Rule a Kingdom and fight for the Shogunate")
+game5 = Games.new("Read Dead Redemption", "Action", "It's Grand Theft Auto Cowboys. Yeahaw")
+game6 = Games.new("Gears of War", "Action", "Defend humanity against monsters that live underground")
+game7 = Games.new("Half Life 2", "Action", "Follow Gordon Freeman's fight to save humanity")
+game8 = Games.new("Fallout New Vegas", "RPG", "Find the guy that shot you in the head. Shoot him back")
+game9 = Games.new("Elder Scrolls V Skyrim", "RPG", "As the Dragonborn, kill other dragons")
+game10 = Games.new("The Witcher 3", "RPG", "Stop the Wild Hunt, Play GWENT")
+game11 = Games.new("Call of Duty: Black Ops" , "Action" , "INSERT DESC HERE")
+game12 = Games.new("Call of Duty: Black Ops 2" , "Action" , "INSERT DESC HERE")
+game13 = Games.new("Call of Duty: World at War" , "Action" , "INSERT DESC HERE")
+game14 = Games.new("Outlast" , "Horror" , "INSERT DESC HERE")
+game15 = Games.new("Outlast" , "Horror" , "INSERT DESC HERE")
+game16 = Games.new("The Evil Within 2" , "Horror" , "INSERT DESC HERE")
+game17 = Games.new("Arma 3" , "Action" , "INSERT DESC HERE")
+game18 = Games.new("Squads" , "Action" , "INSERT DESC HERE")
+game19 = Games.new("Rust" , "Survival" , "INSERT DESC HERE")
+game20 = Games.new("Minecraft" , "Survival" , "INSERT DESC HERE")
+
+#Populating the array
+game_array.push(game1, game2, game3, game4, game5, game6, game7, game8, game9, 
+game10, game11, game12, game13, game14, game15, game16, game17, game18, game19, game20)
 
 
 #The Header Print. This Will be the Greeting message. It will also prompt entry into application
@@ -20,14 +46,17 @@ enterinput = gets.chomp
 loop do
 if enterinput == "e"
    welcome_message
-   puts question  
+   puts question
+   puts options  
 end
 
 userinput = gets.chomp
 
 if userinput == "a"
     welcome_message
-    puts "action"
+    # puts "Confirm your an admin"
+    # puts "Please enter Pin Code"
+    # pincode = 1234
     STDIN.getch
  
     elsif userinput == "h"
@@ -48,6 +77,11 @@ if userinput == "a"
     elsif userinput == "o"
         welcome_message
         puts "open world"
+        STDIN.getch
+
+    elsif userinput == "r"
+        welcome_message
+        puts "We recommend #{game_array.sample}"
         STDIN.getch
 
     else userinput == "q"
